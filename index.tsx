3d4cf6ad57cc4ee415de3f65ef0b86d644d0371a
sitek94 <msitkowski94@gmail.com>
2021-01-06 19:35:12 +0100

    implement rerenders and `React.useState`

--- index.tsx
+++ index.tsx
@@ -1,27 +1,64 @@
-const App = () => (
-  <div className="main" width="100">
-    <h1>Hello world!</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt magni
-      ullam sit reiciendis provident, molestiae voluptatibus voluptatum illum
-      error nemo nesciunt ipsam dolores quibusdam, excepturi dolorum accusamus
-      mollitia, quidem quam.
-      <span>1000</span>
-    </p>
-  </div>
-);
+const App = () => {
+  const [name, setName] = React.useState('Maciek');
+
+  return (
+    <div className="main">
+      <div>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onchange={(e) => setName(e.target.value)}
+          />
+        </label>
+      </div>
+      <h1>Hello, {name}</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt magni
+        ullam sit reiciendis provident, molestiae voluptatibus voluptatum illum
+        error nemo nesciunt ipsam dolores quibusdam, excepturi dolorum accusamus
+        mollitia, quidem quam.
+        <span>1000</span>
+      </p>
+    </div>
+  );
+};
 
 /**
  * REACT
  */
+
+let state;
+
 const React = {
+  useState: (initialState) => {
+    state = state || initialState;
+    const setState = (newState) => {
+      console.log('state was', state, 'state is', newState)
+
+      state = newState;
+
+      render();
+    };
+
+    return [state, setState] as const;
+  },
   createElement: (fnOrTag, props, ...children) => {
     if (typeof fnOrTag === 'function') {
-      /* It has to return Virtual DOM */
-      return fnOrTag();
+
+      // Return Virtual DOM
+      return {
+        ...fnOrTag(props),
+
+        // Render method that allows us to rerender
+        render: () => fnOrTag(props),
+      };
     }
 
-    return { tag: fnOrTag /* <- it's a tag */, props: { ...props, children } };
+    return { 
+      tag: fnOrTag, // <- It's going to be a tag
+      props: { ...props, children } };
   },
 };
 
@@ -30,7 +67,6 @@ const React = {
  */
 const ReactDOM = {
   render: (reactElement, domRoot) => {
-
     // Children are `null` sometimes
     if (!reactElement) {
       return;
@@ -44,6 +80,10 @@ const ReactDOM = {
 
     const rootEl = document.createElement(reactElement.tag);
     const props = reactElement.props ?? {};
+
+    // Rerender if render method exists
+    reactElement.render?.(props);
+
     const propKeys = Object.keys(props).filter((p) => p !== 'children');
     const children = props.children ?? [];
 
@@ -62,4 +102,12 @@ const ReactDOM = {
   },
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+// Renders `<App>` into the `#root` making sure that its empty first
+const render = () => {
+  const parent = document.querySelector('#root');
+  parent?.firstChild?.remove();
+
+  ReactDOM.render(<App />, parent);
+};
+
+render();
