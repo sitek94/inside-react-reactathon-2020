7e2a5207d7e29813cc79c1c8a570856aa2c845c8
sitek94 <msitkowski94@gmail.com>
2021-01-06 18:56:20 +0100

    implement

--- index.tsx
+++ index.tsx
@@ -1,15 +1,19 @@
 const App = () => (
-  <div className="main">
+  <div className="main" width="100">
     <h1>Hello world!</h1>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt magni
       ullam sit reiciendis provident, molestiae voluptatibus voluptatum illum
       error nemo nesciunt ipsam dolores quibusdam, excepturi dolorum accusamus
       mollitia, quidem quam.
+      <span>1000</span>
     </p>
   </div>
 );
 
+/**
+ * REACT
+ */
 const React = {
   createElement: (fnOrTag, props, ...children) => {
     if (typeof fnOrTag === 'function') {
@@ -21,9 +25,40 @@ const React = {
   },
 };
 
+/**
+ * REACT DOM
+ */
 const ReactDOM = {
-  render: (reactElement: any) => {
-    console.log(reactElement);
+  render: (reactElement, domRoot) => {
+
+    // Children are `null` sometimes
+    if (!reactElement) {
+      return;
+    }
+
+    // Text nodes
+    if (typeof reactElement === 'string' || typeof reactElement === 'number') {
+      domRoot.appendChild(document.createTextNode(String(reactElement)));
+      return;
+    }
+
+    const rootEl = document.createElement(reactElement.tag);
+    const props = reactElement.props ?? {};
+    const propKeys = Object.keys(props).filter((p) => p !== 'children');
+    const children = props.children ?? [];
+
+    // Map properties onto the root element
+    propKeys.forEach((k) => {
+      rootEl[k] = props[k];
+    });
+
+    // Render children inside root element
+    children.forEach((child) => {
+      ReactDOM.render(child, rootEl);
+    });
+
+    // Append root element to the DOM
+    domRoot.appendChild(rootEl);
   },
 };
 
